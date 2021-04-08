d641090a443010e11c903dadffbf568fa24181b0
xyz600 <xyz600600@gmail.com>
2021-04-09 01:23:46 +0900

    implement simulated annealing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use rand::distributions::{Distribution, Uniform};
+use rand::rngs::ThreadRng;
+use rand::Rng;
 use std::env;
 use std::fs;
+use std::time::Instant;
 
 struct Graph {
     edges: Vec<Vec<(usize, i64)>>,
@@ -54,8 +58,114 @@ fn load_data() {
     assert!(graph.n == 800);
 }
 
+fn calculate_energy(graph: &Graph, solution: &Vec<i64>) -> i64 {
+    let mut energy = 0;
+    for i in 0..graph.n {
+        for (j, w) in &graph.edges[i] {
+            if i < *j {
+                energy += w * (1 - solution[i] * solution[*j]) / 2;
+            }
+        }
+    }
+    energy
+}
+
+fn calculate_energy_diff(graph: &Graph, solution: &Vec<i64>, pos: usize) -> i64 {
+    solution[pos]
+        * graph.edges[pos]
+            .iter()
+            .map(|(j, w)| *w * solution[*j])
+            .sum::<i64>()
+}
+
+#[test]
+fn energy_diff_test() {
+    let data = "data/G1";
+    let graph = load_problem(data);
+
+    let mut solution = vec![0i64; graph.n];
+    for i in 0..graph.n {
+        solution[i] = if i % 2 == 0 { 1 } else { -1 };
+    }
+
+    let mut rng = rand::thread_rng();
+    let uniform_rand = Uniform::from(0..graph.n);
+
+    for _ in 0..10000 {
+        let pos = uniform_rand.sample(&mut rng);
+        let energy_before = calculate_energy(&graph, &solution);
+        let energy_diff = calculate_energy_diff(&graph, &solution, pos);
+        solution[pos] *= -1;
+        let energy_after = calculate_energy(&graph, &solution);
+        assert_eq!(energy_before + energy_diff, energy_after);
+    }
+}
+
+fn accept(energy_diff: i64, progress: f64, rng: &mut ThreadRng) -> bool {
+    if energy_diff >= 0 {
+        true
+    } else {
+        let prob = (energy_diff as f64 * progress * 1e-4).exp();
+        rng.gen::<f64>() <= prob
+    }
+}
+
 fn main() {
+    let start_time = Instant::now();
+    let timeout = 5000; // [ms]
+
     let args: Vec<String> = env::args().collect();
     let graph = load_problem(&args[1].to_string());
-    println!("{}", graph.n);
+
+    // max-cut
+    // \sum_ij G_ij (1 - x_i x_j)
+    // x_i \in {-1, 1}
+
+    // initial solution
+    let mut solution = vec![0i64; graph.n];
+    for i in 0..graph.n {
+        solution[i] = if i % 2 == 0 { 1 } else { -1 };
+    }
+
+    let mut energy = calculate_energy(&graph, &solution);
+
+    let mut best_energy = energy;
+    let mut best_solution = solution.clone();
+
+    let mut counter = 0u64;
+    let check_frequency = 8192;
+    let mut progress = 0f64;
+
+    let mut rng = rand::thread_rng();
+    let uniform_rand = Uniform::from(0..graph.n);
+
+    loop {
+        // change state
+        let pos = uniform_rand.sample(&mut rng);
+        let energy_diff = calculate_energy_diff(&graph, &solution, pos);
+        if accept(energy_diff, progress, &mut rng) {
+            solution[pos] *= -1;
+            energy += energy_diff;
+        }
+
+        if best_energy < energy {
+            best_energy = energy;
+            best_solution = solution.clone();
+        }
+
+        counter += 1;
+        if counter % check_frequency == 0 {
+            let end = start_time.elapsed().as_millis();
+            if end > timeout {
+                break;
+            }
+            progress = (end as f64) / (timeout as f64);
+        }
+    }
+    println!("{}", counter);
+    println!("energy = {}", best_energy);
+    // println!("solution = {:?}", best_solution);
+
+    let last_energy = calculate_energy(&graph, &best_solution);
+    println!("energy = {}", last_energy);
 }
