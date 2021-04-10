87837ebbcba6bd074ca2540f2d017fd8f7618085
yuichi.sayama <yuichi.sayama@fixstars.com>
2021-04-10 18:15:46 +0900

    create markdown table

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use rand::Rng;
 use rustc_serialize::json;
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use std::time::Instant;
 
 struct Graph {
@@ -162,6 +164,52 @@ fn simulated_annealing(graph: &Graph, timeout: u128) -> (i64, Vec<i64>) {
     (best_energy, best_solution)
 }
 
+struct TableWriter {
+    table: Vec<Vec<String>>,
+}
+
+impl TableWriter {
+    fn new(header: Vec<String>) -> TableWriter {
+        let mut ret = TableWriter {
+            table: vec![header],
+        };
+        ret.table[0].insert(0, String::new());
+        ret
+    }
+
+    fn append_row(&mut self) {
+        self.table.push(vec![String::new(); self.table[0].len()]);
+    }
+
+    fn save(&self, path: &str) {
+        let to_oneline = |row: &Vec<String>| "|".to_string() + row.join("|").as_str() + "|";
+
+        let write_line = |row: &Vec<String>, writer: &mut BufWriter<File>| {
+            if let Err(error) = writer.write((to_oneline(row) + "\n").as_bytes()) {
+                println!("error while save: {}", error);
+                panic!();
+            }
+        };
+
+        match File::create(path) {
+            Ok(file) => {
+                let mut writer = BufWriter::new(file);
+                write_line(&self.table[0], &mut writer);
+
+                let delimiter = vec!["----".to_string(); self.table[0].len()];
+                write_line(&delimiter, &mut writer);
+
+                for row in self.table.iter().skip(1) {
+                    write_line(&row, &mut writer);
+                }
+            }
+            Err(error) => {
+                println!("{}", error);
+            }
+        }
+    }
+}
+
 #[derive(Debug, RustcDecodable)]
 struct InstanceSettings {
     name: String,
@@ -185,6 +233,8 @@ fn main() {
     let maybe_settings =
         json::decode::<ExperimentalSettings>(fs::read_to_string(config_path).unwrap().as_str());
 
+    let mut writer = TableWriter::new(vec!["timeout[ms]".to_string(), "score".to_string()]);
+
     match maybe_settings {
         Ok(settings) => {
             let rootpath = &settings.rootpath;
@@ -201,7 +251,12 @@ fn main() {
                 // println!("solution = {:?}", best_solution);
                 let last_energy = calculate_energy(&graph, &best_solution);
                 println!("energy for check = {}", last_energy);
+                writer.append_row();
+                writer.table.last_mut().unwrap()[0] = instance.name.clone();
+                writer.table.last_mut().unwrap()[1] = instance.timeout.to_string();
+                writer.table.last_mut().unwrap()[2] = best_energy.to_string();
             }
+            writer.save("result.md");
         }
         Err(error) => {
             println!("{}", error);
